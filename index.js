244c9146512b9f6498d4ee54d872372fca93caff
Devansh D <devanshdalal2@gmail.com>
2016-11-24 17:15:53 +0000

    Added a Procfile.

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 var express = require('express');
 var app = express();
+var dateFormat = require('dateformat');
+var moment = require('moment');
 
 app.set('port', (process.env.PORT || 5000));
 
@@ -9,12 +11,36 @@ app.use(express.static(__dirname + '/public'));
 app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
 
-app.get('/', function(request, response) {
-  response.render('pages/index');
-});
 
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
 });
+
+app.get('/:NAME', function (req, res) {
+  // console.warn(req);
+  
+  var timestamp={
+    unix:null,
+    natural:null
+  };
+  
+  var date_format="mmmm d, yyyy";
+  
+  var value=req.params.NAME;
+  console.warn(value);
+  
+  if( value.match(/^[0-9]+$/) != null ){
+    var d = new Date(value*1000);
+    timestamp.unix=value;
+    timestamp.natural=dateFormat(d,date_format);
+  }else if( moment(value, "LL" , true).isValid() ){
+    timestamp.natural=value;
+    timestamp.unix =  new Date(value).getTime() / 1000;
+    moment();
+  }else{
+    
+  }
+  res.send(JSON.stringify(timestamp)+"\n");
+})
 
 
